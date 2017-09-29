f1f43032bbae674a208cdfec46710dae9285a9e0
Anatoliy Popov <popov@naks.ru>
2017-09-29 16:36:44 +0300

    less3 normal not ready

--- lesson03/home_work/hw03_easy.py
+++ lesson03/home_work/hw03_easy.py
@@ -3,40 +3,62 @@
 # до кол-ва знаков (кол-во знаков передается вторым аргументом).
 # Округление должно происходить по математическим правилам (0.6 --> 1, 0.4 --> 0).
 # Для решения задачи не используйте встроенные функции и функции из модуля math.
-
 def my_round(number, ndigits):
     rounded_arr = str(number)
     rounded_arr = rounded_arr.split('.')
     res = rounded_arr[1]
     for i in res:
-        if res.index(i) <= ndigits:
-            result = res[:ndigits]
-    
-    if int(result[-1])>=5:
-        print(int(result[-2])+1)
-    else:
-        print(int(result[-2])-1)
-    print (result)
-    
-            
-    return result
-
-
+        if res.index(i) < ndigits and int(res[res.index(i)+1]) >= 5:
+            result = int(res[:ndigits])+1
+        elif res.index(i) < ndigits and int(res[res.index(i)+1]) < 5:
+            result = int(res[:ndigits])
+        elif ndigits == 0:
+            result = 0
+    rounded_arr[1] = str(result)
+    if result%10**ndigits==0 and ndigits!=0:
+        rounded_arr[0]=int(rounded_arr[0])+1
+        rounded_arr[0] = str(rounded_arr[0])
+        rounded_arr.pop(1)
+    rounded_arr = ".".join(rounded_arr)
+    return float(rounded_arr)
+print('my_round:')
 print(my_round(2.1234567, 5))
-# print(my_round(2.1999967, 5))
-# print(my_round(2.9999967, 5))
-
+print(my_round(2.1999967, 5))
+print(my_round(2.9999967, 5))
+#для сравнения:
+print('round:')
+print(round(2.1234567, 5))
+print(my_round(2.1999967, 5))
+print(round(2.9999967, 5))
 
 # Задание-2:
 # Дан шестизначный номер билета. Определить, является ли билет счастливым.
 # Решение реализовать в виде функции.
 # Билет считается счастливым, если сумма его первых и последних цифр равны.
 # !!!P.S.: функция не должна НИЧЕГО print'ить
-
-# def lucky_ticket(ticket_number):
-#     pass
-#    
-#    
-# print(lucky_ticket(123006))
-# print(lucky_ticket(12321))
-# print(lucky_ticket(436751))
+def lucky_ticket(ticket_number):
+    ticket_number=str(ticket_number)
+    arr1=[]
+    arr2=[]
+    if len(ticket_number)==6:
+        for i in range(0, int(len(ticket_number)/2)):
+            if str.isdigit(str(i)):
+                arr1.append(int(ticket_number[i]))
+            else:
+                result = False
+        for i in range(int(len(ticket_number)/2), int(len(ticket_number))):
+            if str.isdigit(str(i)):
+                arr2.append(int(ticket_number[i]))
+            else:
+                result = False
+        if sum(arr1) == sum(arr2):
+            result = True
+        else:
+            result = False
+    if len(ticket_number)!=6:
+        result = False
+    return result
+    
+print(lucky_ticket(123006))
+print(lucky_ticket(12321))
+print(lucky_ticket(436751))
--- lesson03/home_work/hw03_normal.py
+++ lesson03/home_work/hw03_normal.py
@@ -3,25 +3,73 @@
 # Первыми элементами ряда считать цифры 1 1
 
 def fibonacci(n, m):
-    pass
+    fib_arr = [1, 1]
+    fib1 = 1
+    fib2 = 1
+    i = 2
+    while i < m:
+        fib_sum = fib2+fib1
+        fib1 = fib2
+        fib2 = fib_sum
+        i += 1
+        fib_arr.append(fib_sum)
+    return fib_arr[n:]
+
+print(fibonacci(5, 15))
 
 # Задача-2:
 # Напишите функцию, сортирующую принимаемый список по возрастанию.
 # Для сортировки используйте любой алгоритм (например пузырьковый).
 # Для решения данной задачи нельзя использовать встроенную функцию и метод sort()
 
-
 def sort_to_max(origin_list):
-    pass
+    for blob in range (len(origin_list)-1, 0, -1):
+        for i in range (blob):
+            if origin_list[i]>origin_list[i+1]:
+                temp = origin_list[i]
+                origin_list[i] = origin_list[i+1]
+                origin_list[i+1]=temp
+    return origin_list
 
-sort_to_max([2, 10, -12, 2.5, 20, -11, 4, 4, 0])
+print(sort_to_max([2, 10, -12, 2.5, 20, -11, 4, 4, 0]))
 
 # Задача-3:
 # Напишите собственную реализацию стандартной функции filter.
 # Разумеется, внутри нельзя использовать саму функцию filter.
 
+def my_filter(f, iterable):
+    if f != None:
+        result = [item for item in iterable if f(item)]
+    else:
+        result = [item for item in iterable if item]
+    return result
+
+def f(x):
+    return x%2!=0
+
+def f1(x):
+    return x<0
+
+def f2(x):
+    return str.isdigit(str(x))==False
+
+print(my_filter(f, [14, 15, 16, 18, 19, 21]))
+print(my_filter(f1, [-14, 15, -16, 18, 19, 21]))
+print(my_filter(f2, [14, 19, 'test', 21, 'woop']))
+print(my_filter(None, [-14, 19, 21, 'test', 'woop']))
 
 # Задача-4:
 # Даны четыре точки А1(х1, у1), А2(x2 ,у2), А3(x3 , у3), А4(х4, у4).
 # Определить, будут ли они вершинами параллелограмма.
 
+A1=(1, 1)
+A2=(2, 4)
+A3=(7, 4)
+A4=(6, 1)
+
+def is_par(bl, tl, tr, br):
+    if br[0]-bl[0] == tr[0]-tl[0] and tl[1]-bl[1] == tr[1]-br[1]:
+        return True
+    else:
+        return False
+print(is_par(A1, A2, A3, A4))
